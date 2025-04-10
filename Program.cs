3acff14d170f134548edee8e8b3e2551e1a0908f
mckaradachi <markokapranov11@gmail.com>
2025-04-10 23:15:44 +0300

    Wrote Time tests

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿using System.Diagnostics;
+using System.Text;
 using KruskalAlgorithm;
 
 Stopwatch graphCreate = new Stopwatch();
 graphCreate.Start();
-Graph graph = GraphFactory.CreateGraph(100, 3);
+Graph graph = GraphFactory.CreateGraph(1000, 1);
 graphCreate.Stop();
 
 while (!graph.IsConnected())
 {
-    graph = GraphFactory.CreateGraph(7, 0.5);
+    graph = GraphFactory.CreateGraph(1000, 1);
 }
 
 var AdjacencyList = graph.GetAdjacencyList();
@@ -23,6 +24,7 @@ for (int i = 0; i < AdjacencyMatrix.GetLength(0); i++)
     Console.WriteLine();
 }
 
+
 foreach (var pair in AdjacencyList)
 {
     Console.WriteLine(pair.Key.Id);
@@ -35,17 +37,16 @@ foreach (var pair in AdjacencyList)
 
 Stopwatch algorhytmTime = new Stopwatch();
 algorhytmTime.Start();
-var mintree = KruskalsAlgorithm.MinimumSpanningTreeOnAdjacencyList(graph);
-Console.WriteLine($"Minimum spanning tree on list is  {mintree.Item1}");
 var mintreeonmatrix = KruskalsAlgorithm.MinimumSpanningTreeOnAdjacencyMatrix(graph);
 Console.WriteLine($"Minimum spanning tree on matrix is  {mintreeonmatrix.Item1}");
-Console.WriteLine($"Minimum spanning tree is  {mintree.Item1}");
+var mintree = KruskalsAlgorithm.MinimumSpanningTreeOnAdjacencyList(graph);
+Console.WriteLine($"Minimum spanning tree on list is  {mintree.Item1}");
 algorhytmTime.Stop();
+Console.WriteLine($"Minimum spanning tree is  {mintree.Item1}");
 
 
-var AdjacencyListmin = mintree.Item2.GetAdjacencyList();
 var AdjacencyListMimOnMatrix = mintreeonmatrix.Item2.GetAdjacencyList();
-
+var AdjacencyListmin = mintree.Item2.GetAdjacencyList();
 
 Console.WriteLine("Adjacency list from MinimumSpanningTreeOnAdjacencyList");
 foreach (var pair in AdjacencyListmin)
@@ -59,6 +60,7 @@ foreach (var pair in AdjacencyListmin)
 }
 
 Console.WriteLine("Adjacency list from MinimumSpanningTreeOnAdjacencyMatrix");
+
 foreach (var pair in AdjacencyListMimOnMatrix)
 {
     Console.WriteLine(pair.Key.Id);
@@ -68,7 +70,40 @@ foreach (var pair in AdjacencyListMimOnMatrix)
     }
     Console.WriteLine("\n");
 }
-   
 
 Console.WriteLine($"Graph was creating for {graphCreate.ElapsedMilliseconds} mls");
 Console.WriteLine($"Kruskal algorhytm took {algorhytmTime.ElapsedMilliseconds} mls");
+/*
+string projectRoot = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+string filePath = Path.Combine(projectRoot, "dataMatrix.csv");
+StringBuilder csvContent = new StringBuilder();
+csvContent.AppendLine("Size,Density,№1,№2,№3,№4,№5,№6,№7,№8,№9,№10,№11,№12,№13,№14,№15,№16,№17,№18,№19,№20,Average");
+for (int verticeNumber = 20;verticeNumber <= 200; verticeNumber += 20 ) 
+{
+    for (double density = 0.2; density <= 1; density += 0.2)
+    {
+        StringBuilder times = new StringBuilder();
+        
+        for (int i = 0; i < 20; i++)
+        {
+            Graph graph = GraphFactory.CreateGraph(verticeNumber, density);
+            while (!graph.IsConnected())
+            {
+                graph = GraphFactory.CreateGraph(verticeNumber, density);
+            }
+            Stopwatch algorhytmTime = new Stopwatch();
+            algorhytmTime.Start();
+            var mintree = KruskalsAlgorithm.MinimumSpanningTreeOnAdjacencyMatrix(graph);
+            algorhytmTime.Stop();
+            var elapsedTime = Math.Round(algorhytmTime.Elapsed.TotalMilliseconds, 2);
+            times.Append((elapsedTime).ToString().Replace(",", ".") + ",");
+            Console.WriteLine(elapsedTime+ "mls");
+        }
+
+        var average = times.ToString().Split(",").Take(20).Select(x => decimal.Parse(x.Replace(".", ","))).Average(x => x).ToString().Replace(",", ".");
+        
+        csvContent.AppendLine($"{verticeNumber},{Math.Round(density, 1).ToString().Replace(",", ".")},"+ times.ToString()+ average);
+    }
+}
+File.WriteAllText(filePath, csvContent.ToString());
+*/

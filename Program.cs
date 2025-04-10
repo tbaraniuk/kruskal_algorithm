3e24fe4c0bc13c5ebcaa46f945375144e3109715
mckaradachi <markokapranov11@gmail.com>
2025-04-10 03:00:43 +0300

    banchmark set

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿using KruskalAlgorithm;
+﻿using System.Diagnostics;
+using KruskalAlgorithm;
 
-Graph graph = GraphFactory.CreateGraph(7, 0.5);
+Stopwatch graphCreate = new Stopwatch();
+graphCreate.Start();
+Graph graph = GraphFactory.CreateGraph(100, 3);
+graphCreate.Stop();
 
 while (!graph.IsConnected())
 {
-    graph = GraphFactory.CreateGraph(7, 0.5);
+    graph = GraphFactory.CreateGraph(100, 3);
 }
 
 var AdjacencyList = graph.GetAdjacencyList();
@@ -29,8 +33,11 @@ foreach (var pair in AdjacencyList)
     Console.WriteLine("\n");
 }
 
+Stopwatch algorhytmTime = new Stopwatch();
+algorhytmTime.Start();
 var mintree = KruskalsAlgorithm.MinimumSpanningTreeOnAdjacencyList(graph);
 Console.WriteLine($"Minimum spanning tree is  {mintree.Item1}");
+algorhytmTime.Stop();
 
 
 var AdjacencyListmin = mintree.Item2.GetAdjacencyList();
@@ -43,4 +50,7 @@ foreach (var pair in AdjacencyListmin)
         Console.Write($"{valueTuple.Item1.Id} - {valueTuple.Item2}\n");
     }
     Console.WriteLine("\n");
-}
+   
+} 
+Console.WriteLine($"Graph was creating for {graphCreate.ElapsedMilliseconds} mls");
+Console.WriteLine($"Kruskal algorhytm took {algorhytmTime.ElapsedMilliseconds} mls");
